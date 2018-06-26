4002323794c3cec381ea14e001016bd96014bb64
hmesander <haleymesander@gmail.com>
2018-06-26 13:42:14 -0600

    TESTS for customer show endpoint

--- spec/requests/api/v1/customers/customers_request_spec.rb
+++ spec/requests/api/v1/customers/customers_request_spec.rb
@@ -12,4 +12,15 @@ describe 'Customers API' do
 
     expect(customers.length).to eq(5)
   end
+
+  it 'can get one customer by its id' do
+    id = create(:customer).id
+
+    get "/api/v1/customers/#{id}"
+
+    customer = JSON.parse(response.body)
+
+    expect(response).to have_http_status(200)
+    expect(customer['id']).to eq(id)
+  end
 end

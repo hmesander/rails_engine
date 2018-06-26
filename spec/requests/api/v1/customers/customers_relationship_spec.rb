f57d74d93e7226a27022249077ac58b98724febc
hmesander <haleymesander@gmail.com>
2018-06-26 14:31:54 -0600

    TESTS for customer transactions endpoint

--- spec/requests/api/v1/customers/customers_relationship_spec.rb
+++ spec/requests/api/v1/customers/customers_relationship_spec.rb
@@ -13,4 +13,19 @@ describe 'Customers API' do
 
     expect(invoices.count).to eq(5)
   end
+
+  it 'sends a list of transactions belonging to a customer' do
+    customer = create(:customer)
+    merchant = create(:merchant)
+    invoice = create(:invoice, merchant: merchant, customer: customer)
+    create_list(:transaction, 5, invoice: invoice)
+
+    get "/api/v1/customers/#{customer.id}/transactions"
+
+    expect(response).to have_http_status(200)
+
+    transactions = JSON.parse(response.body)
+
+    expect(transactions.count).to eq(5)
+  end
 end

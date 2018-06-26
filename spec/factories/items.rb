13a452f1d2b0d81c462c4aea5089e6904b728462
zdcthomas <zdcthomas@yahoo.com>
2018-06-26 10:32:50 -0600

    TESTS item find all, find, random

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :item do
-    name "MyString"
-    description "MyText"
+    name Faker::Commerce.product_name
+    description Faker::Coffee.notes
     merchant
     unit_price ""
   end
--- spec/requests/api/v1/item_request_spec.rb
+++ spec/requests/api/v1/item_request_spec.rb
@@ -2,21 +2,55 @@ require 'rails_helper'
 
 describe "Items API" do
   it 'should have a reachable path which sends an index of items' do
-    create(:item)
+    create_list(:item, 3)
 
     get '/api/v1/items'
     expect(response).to be_successful
     items = JSON.parse(response.body)
-    expect(items.count).to eq(1)
+    expect(items.count).to eq(3)
   end
 
-  it 'should return a specific requested item' do
+  it 'should return a specific requested item by id' do
     item_id = create(:item).id
 
     get "/api/v1/items/#{item_id}"
 
     item = JSON.parse(response.body)
+
+    expect(response).to be_successful
     expect(item["id"]).to eq(item_id)
   end
 
+  it 'should return a specific item using find' do
+    item =  create(:item)
+    get "/api/v1/items/find?name=#{item.name}"
+
+    returned_item = JSON.parse(response.body)
+    
+    expect(response).to be_successful
+    expect(returned_item["name"]).tto eq(item.name)
+    expect(returned_item["id"]).tto eq(item.id)
+  end
+
+  it 'should return all items matching a given parameter' do
+    price = 2000
+    create_list(:item, 10, unit_price: price)
+    create_list(:item, 3, unit_price: 10)
+
+    get "/api/v1/merchants/find_all?unit_price=#{price}"
+    returned_items = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(returned_items.count).to eq(10)
+  end
+  it 'should return a random item' do
+    create_list(:item, 10)
+    possible_ids = Item.all.map{|item| item.id}
+
+    get '/api/v1/items/random'
+    item = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(possible_ids).to include(item.id)
+  end
 end

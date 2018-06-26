3ea920e6600a60e3350b59056814517d47c4f86b
hmesander <haleymesander@gmail.com>
2018-06-26 15:01:44 -0600

    TESTS for transaction show endpoint

--- spec/requests/api/v1/transactions/transactions_request_spec.rb
+++ spec/requests/api/v1/transactions/transactions_request_spec.rb
@@ -15,4 +15,19 @@ describe 'Transactions API' do
 
     expect(transactions.length).to eq(3)
   end
+
+  it 'can get one transaction by its id' do
+    customer = create(:customer)
+    merchant = create(:merchant)
+    invoice = create(:invoice, customer: customer, merchant: merchant)
+    transactions = create_list(:transaction, 3, invoice: invoice)
+    id = transactions[1].id
+
+    get "/api/v1/transactions/#{id}"
+
+    transaction = JSON.parse(response.body)
+
+    expect(response).to have_http_status(200)
+    expect(transaction['id']).to eq(id)
+  end
 end

9399b9b61256542a6c9ed9d8e2cc07df12e63424
hmesander <haleymesander@gmail.com>
2018-06-25 19:56:45 -0600

    TESTS merchants show endpoint

--- spec/requests/merchants_request_spec.rb
+++ spec/requests/merchants_request_spec.rb
@@ -12,4 +12,15 @@ describe 'Merchants API' do
 
     expect(merchants.length).to eq(3)
   end
+
+  it 'can get one merchant by its id' do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to have_http_status(200)
+    expect(merchant['id']).to eq(id)
+  end
 end

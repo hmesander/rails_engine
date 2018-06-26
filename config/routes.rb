4d8b93c68d34b285b0d1a6775ea876fa82bb6e7c
hmesander <haleymesander@gmail.com>
2018-06-26 15:02:40 -0600

    Implements transaction show endpoint ATP

--- config/routes.rb
+++ config/routes.rb
@@ -40,7 +40,7 @@ Rails.application.routes.draw do
       end
       resources :invoices, only: [:index, :show]
 
-      resources :transactions, only: [:index]
+      resources :transactions, only: [:index, :show]
     end
   end
 end

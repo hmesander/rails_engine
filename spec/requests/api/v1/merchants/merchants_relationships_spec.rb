4f0d0176eea67601afab98c0ca7d60ebb998c3fc
hmesander <haleymesander@gmail.com>
2018-06-26 10:57:48 -0600

    TESTS merchant/invoices endpoint

--- spec/requests/api/v1/merchants/merchants_relationships_spec.rb
+++ spec/requests/api/v1/merchants/merchants_relationships_spec.rb
@@ -13,4 +13,18 @@ describe 'Merchants API' do
 
     expect(items.count).to eq(5)
   end
+
+  it 'sends a list of invoices belonging to a merchant' do
+    merchant = create(:merchant)
+    customer = create(:customer)
+    create_list(:invoice, 7, merchant_id: merchant.id, customer_id: customer.id)
+
+    get "/api/v1/merchants/#{merchant.id}/invoices"
+
+    expect(response).to have_http_status(200)
+
+    invoices = JSON.parse(response.body)
+
+    expect(invoices.count).to eq(7)
+  end
 end

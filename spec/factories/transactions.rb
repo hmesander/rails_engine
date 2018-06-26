25744dc7811714e589d64be321c1a74da16b046f
zdcthomas <zdcthomas@yahoo.com>
2018-06-26 16:39:14 -0600

    implements better transaction factory

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :transaction do
     invoice
-    credit_card_number ""
+    credit_card_number Faker::Number.number(12)
     credit_card_expiration_date "2018-06-25 17:28:34"
-    result "MyString"
+    result Faker::Ancient.god
   end
 end

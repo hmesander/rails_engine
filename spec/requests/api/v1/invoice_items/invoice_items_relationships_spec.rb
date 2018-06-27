39dfa7806df73d17f70e0c9621f550dc0ed3c44d
hmesander <haleymesander@gmail.com>
2018-06-26 21:03:46 -0600

    TESTS for invoice item/item relationship endpoint

--- spec/requests/api/v1/invoice_items/invoice_items_relationships_spec.rb
+++ spec/requests/api/v1/invoice_items/invoice_items_relationships_spec.rb
@@ -16,4 +16,20 @@ describe 'Invoice Items API' do
 
     expect(returned['id']).to eq(invoice.id)
   end
+
+  it 'sends the item associated with an invoice item' do
+    merchant = create(:merchant)
+    customer = create(:customer)
+    invoice = create(:invoice, merchant: merchant, customer: customer)
+    item = create(:item, merchant: merchant)
+    invoice_item = create(:invoice_item, item: item, invoice: invoice)
+
+    get "/api/v1/invoice_items/#{invoice_item.id}/item"
+
+    expect(response).to have_http_status(200)
+
+    returned = JSON.parse(response.body)
+
+    expect(returned['id']).to eq(item.id)
+  end
 end

917748f984f0274b6e329617d41011a1dcb616b5
hmesander <haleymesander@gmail.com>
2018-06-27 16:53:07 -0600

    Corrects to make all tests pass after rebase

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
-<<<<<<< HEAD
-=======
   has_many :invoices, through: :invoice_items
->>>>>>> WIP total revenue for merchant
 end

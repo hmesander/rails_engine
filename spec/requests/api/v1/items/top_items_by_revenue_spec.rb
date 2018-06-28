fbc6fb24c167f7afc0666aa57fc1506b3d99b131
zdcthomas <zdcthomas@yahoo.com>
2018-06-28 09:23:46 -0600

    TESTS items by revenume

---
+++ spec/requests/api/v1/items/top_items_by_revenue_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+describe "Top Items by revenue" do
+  it 'should return the top specified number of items ranked by revenue' do
+    item1 = create(:item, name:"Top Item")
+    item2 = create(:item, name:"Top Item")
+    item3 = create(:item, name:"Top Item")
+    item4 = create(:item, name:"Top Item")
+
+    invoice = create(:invoice)
+
+    create_list(invoice_items, 5, quantity: 5, unit_price: 1000, item: item1, invoice: invoice) 
+    create_list(invoice_items, 5, quantity: 3, unit_price: 900, item: item2, invoice: invoice) 
+    create_list(invoice_items, 5, quantity: 2, unit_price: 600, item: item3, invoice: invoice) 
+    create_list(invoice_items, 5, quantity: 1, unit_price: 400, item: item4, invoice: invoice) 
+
+    transaction = create(:transaction, invoice: invoice, result: "success")
+
+    get '/api/v1/items/most_revenue?quantity=2'
+
+    items_returned = JSON.parse(response.body)
+
+    expect(items_returned.size).to eq(2)
+    expect(items_returned[0]["name"]).to eq(item1.name)
+  end
+end

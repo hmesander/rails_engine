3b3a0472c952414182c4bd69e650f825f25c2127
hmesander <haleymesander@gmail.com>
2018-06-28 19:49:58 -0600

    ADDS validation tests and validations for all models

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
+  describe 'Validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:unit_price) }
+  end
+
   describe 'Relationships' do
     it { should have_many(:invoices) }
     it { should have_many(:invoice_items) }
